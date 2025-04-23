f1517b42c1f20e221d6abec05e8961edf30f18ad
Varsha567 <varsha.gubbala@gmail.com>
2025-04-23 22:57:18 +0530

    deploy changes

--- server/server.js
+++ server/server.js
@@ -1,30 +1,41 @@
-
-
-// Add these middlewares
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth');
-const studentRoutes = require('./routes/studentRoutes');
 require('dotenv').config();
+
+
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
+const studentRoutes = require("./routes/studentRoutes");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
 
-app.use(cors( {
-  origin: 'http://localhost:3000', // Update with your client's URL
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-connectDB();
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV === "production") {
+  const buildPath = path.join(__dirname, "client", "build");
+  app.use(express.static(buildPath));
+
+  // any GET request not handled above returns React's index.html
+  app.get("*", (_, res) =>
+    res.sendFile(path.join(buildPath, "index.html"))
+  );
+}
+
