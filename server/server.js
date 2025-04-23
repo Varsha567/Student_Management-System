1c6f779d92e23967fefce81cf288a484fea6d551
Varsha567 <varsha.gubbala@gmail.com>
2025-04-24 00:36:29 +0530

    path removed

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const express = require('express');
 
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const path = require("path");
+
 
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
